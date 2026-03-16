3040c71d646433903d7cbe73a95f8fe1e8d94af7
Tamás Péter <tamaspeti3451@gmail.com>
2026-03-16 14:01:28 +0100

    Implemented AABB Gpu-Driven culling test!

--- SynapseEngine/Engine/Scene/TestScene.h
+++ SynapseEngine/Engine/Scene/TestScene.h
@@ -66,7 +66,7 @@ namespace Syn
             registry->GetPool<ModelComponent>()->SetCategory(bistroEntity, StorageCategory::Static);
 
             // Random Geometry
-            for (int i = 0; i < 10000; i++)
+            for (int i = 0; i < 100000; i++)
             {
                 EntityID e = registry->CreateEntity();
                 registry->AddComponent<TransformComponent>(e);
