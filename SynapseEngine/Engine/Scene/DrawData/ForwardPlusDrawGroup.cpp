17ab230f836e77d750e252c98c8192062c771534
Tamás Péter <tamaspeti3451@gmail.com>
2026-05-08 19:22:07 +0200

    Small scene changes

--- SynapseEngine/Engine/Scene/DrawData/ForwardPlusDrawGroup.cpp
+++ SynapseEngine/Engine/Scene/DrawData/ForwardPlusDrawGroup.cpp
@@ -42,7 +42,7 @@ namespace Syn
         tileGridBuffer.UpdateCapacity(frameIndex, gridX * gridY);
         clusterListBuffer.UpdateCapacity(frameIndex, maxClusters);
 
-        uint32_t expectedAverageLightsPerCluster = 64;
+        uint32_t expectedAverageLightsPerCluster = 1024;
         uint32_t totalLightIndices = maxClusters * expectedAverageLightsPerCluster;
 
         pointLightIndexBuffer.UpdateCapacity(frameIndex, totalLightIndices);
