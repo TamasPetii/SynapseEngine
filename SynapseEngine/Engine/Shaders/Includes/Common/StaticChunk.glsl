2e757af059d938cffabaae7b49cb63768e3f8c07
Tamás Péter <tamaspeti3451@gmail.com>
2026-05-15 14:47:49 +0200

    Implemented final static chunk culling compute shader

--- SynapseEngine/Engine/Shaders/Includes/Common/StaticChunk.glsl
+++ SynapseEngine/Engine/Shaders/Includes/Common/StaticChunk.glsl
@@ -10,10 +10,10 @@ struct StaticChunk {
     uint entityCount;
 };
 
-layout(buffer_reference, std430) readonly restrict buffer StaticChunkBuffer { 
-    StaticChunk data[]; 
-};
+layout(buffer_reference, std430) readonly restrict buffer StaticChunkBuffer { StaticChunk data[]; };
+layout(buffer_reference, std430) restrict buffer VisibleChunkList { uint data[]; };
 
 #define GET_STATIC_CHUNK(addr, idx)     StaticChunkBuffer(addr).data[idx]
+#define GET_VISIBLE_CHUNK(addr, idx)    VisibleChunkList(addr).data[idx]
 
 #endif
